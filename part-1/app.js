0c4dbd7aad3219bb80d62a37ab1a5fdfee23d9f7
eobaah <eobaah@gmail.com>
2017-08-14 11:02:05 -0700

    part 1 complete

--- part-1/app.js
+++ part-1/app.js
@@ -1,10 +1,13 @@
 const express = require( 'express' )
 const app = express()
+const bodyParser = require('body-parser')
 const morgan = require( 'morgan' )
 const port = 3000
 
+const jsonParser = bodyParser.json()
 
 app.use( morgan('combined') )
+
 
 app.get( '/api/days/:day', ( request, response ) => {
   let day = request.params.day.toLowerCase()
@@ -19,8 +22,25 @@ app.get( '/api/days/:day', ( request, response ) => {
   }
   !daysOfWeek[day] ?
   response.status(400).send( `response: '${day}' is not a valid day!` ) :
-  response.status(200).send( `response: ${daysOfWeek[day]}` )
+  response.status(200)
+    .set('Content-Type', 'text/plain')
+    .send( `response: ${daysOfWeek[day]}` )
 })
+
+app.post( '/api/array/concat', jsonParser, ( request, response ) => {
+
+  let array1 = request.body.array1
+  let array2 = request.body.array2
+  let result = array1.concat(array2)
+
+  Array.isArray(array1) && Array.isArray(array2) ?
+  response.header('content-type', 'application/json')
+    .json( {"result": result} ) :
+  response.status(400)
+    .header('content-type', 'application/json')
+    .json( {"error": "Input data should be of type Array."} )
+})
+
 
 app.listen( port, () => {
   console.log( `Your app is listening on port ${port}!` )
